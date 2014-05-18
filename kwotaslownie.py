2e2a7e89f3d564ccb20f1ff1a048ae30c32071f3
Michał Sadowski <sq6jnx@hamradio.pl>
2014-05-18 13:04:18 +0200

    PEP8: Usunięto zbędne spacje z końców linii (#3)

--- kwotaslownie.py
+++ kwotaslownie.py
@@ -7,9 +7,9 @@ cosslownie - rzecz słownie, odmiana jako argument ("dwadzieścia niedźwiedzi")
 """
 
 jednostki = [u""] + u"jeden dwa trzy cztery pięć sześć siedem osiem dziewięć".split()
-dziesiatki = [u""] + u"""dziesięć dwadzieścia  trzydzieści czterdzieści 
+dziesiatki = [u""] + u"""dziesięć dwadzieścia  trzydzieści czterdzieści
      pięćdziesiąt sześćdziesiąt siedemdziesiąt osiemdziesiąt dziewięćdziesiąt""".split()
-nastki = u"""dziesięć jedenaście dwanaście trzynaście czternaście piętnaście 
+nastki = u"""dziesięć jedenaście dwanaście trzynaście czternaście piętnaście
         szesnaście siedemnaście osiemnaście dziewiętnaście""".split()
 setki = [u""]+ u"""sto dwieście trzysta czterysta pięćset sześćset siedemset osiemset
               dziewięćset""".split()
@@ -29,7 +29,7 @@ def _slownie3cyfry(liczba):
     dz = (liczba//10) % 10
     se = (liczba//100) % 10
     slowa=[]
-    
+
     if se>0:
         slowa.append(setki[se])
     if dz==1:
@@ -38,7 +38,7 @@ def _slownie3cyfry(liczba):
         if dz>0:
             slowa.append(dziesiatki[dz])
         if je>0:
-            slowa.append(jednostki[je])        
+            slowa.append(jednostki[je])
     retval = " ".join(slowa)
     return retval
 
@@ -46,16 +46,16 @@ def _slownie3cyfry(liczba):
 def _przypadek(liczba):
     je = liczba % 10
     dz = (liczba//10)  % 10
-    
+
     if liczba == 1:
         typ = 0       #jeden tysiąc"
     elif dz==1 and je>1:  # naście tysięcy
-        typ = 2 
+        typ = 2
     elif  2<=je<=4:
         typ = 1       # [k-dziesiąt/set] [dwa/trzy/czery] tysiące
     else:
         typ = 2       # x tysięcy
-    
+
     return typ
 
 def lslownie(liczba):
@@ -66,11 +66,11 @@ def lslownie(liczba):
     while liczba>0:
         trojki.append(liczba % 1000)
         liczba = liczba // 1000
-    slowa = []    
-    for i,n in enumerate(trojki):        
+    slowa = []
+    for i,n in enumerate(trojki):
         if n>0:
             if i>0:
-                p = _przypadek(n)                
+                p = _przypadek(n)
                 w = wielkie[i][p]
                 slowa.append(_slownie3cyfry(n)+u" "+w)
             else:
@@ -80,15 +80,15 @@ def lslownie(liczba):
 
 def cosslownie(liczba,cos):
     """Słownie "ileś cosiów"
-    
+
     liczba - int
     cos - tablica przypadków [coś, cosie, cosiów]"""
-    
+
     return lslownie(liczba)+u" " + cos[_przypadek(liczba)]
 
 def kwotaslownie(liczba, format = 0):
     """Słownie złotych, groszy.
-    
+
     liczba - float, liczba złotych z groszami po przecinku
     format - jesli 0, to grosze w postaci xx/100, słownie w p. przypadku
     """
